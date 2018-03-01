c6ee4a5ef87e86917a1c64ca0b87d5c960430f4e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-01 10:02:33 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -12,18 +12,6 @@
  * @example
  * swing(10, 10)();
  */
-var index = function (A, f, p, d) {
-	if ( A === void 0 ) A = 0;
-	if ( f === void 0 ) f = 1;
-	if ( p === void 0 ) p = 0;
-	if ( d === void 0 ) d = 0;
-
-	return function (t) {
-	if ( t === void 0 ) t = 0;
-
-	return A * Math.cos((t * f) + p) * Math.exp(-d * t);
-;
-}	};
+var index = (A = 0, f = 1, p = 0, d = 0) => (t = 0) => A * Math.cos((t * f) + p) * Math.exp(-d * t)
 
 module.exports = index;
-
