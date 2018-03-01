acae2ebf748b165b4b5dd45b6b465676944f2901
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-01 10:06:56 +0100

    example: cleanup

--- example/index.js
+++ example/index.js
@@ -1,93 +1,56 @@
-(function () {
-'use strict';
+import swing from '../index.mjs'
 
-/**
- * Damped harmonic oscillator
- * @module swing
- * @param {Number} A - Amplitude
- * @param {Number} f - Frequency
- * @param {Number} p - Phase (offset)
- * @param {Number} d - Damping
- * @returns {Number} - Displacement
- * @see {@link https://en.wikipedia.org/wiki/Harmonograph}
- * @example
- * swing(10, 10)();
- */
-var swing = function (A, f, p, d) {
-  if ( A === void 0 ) A = 0;
-  if ( f === void 0 ) f = 1;
-  if ( p === void 0 ) p = 0;
-  if ( d === void 0 ) d = 0;
+const canvas = document.querySelector('canvas')
+const target = canvas.getContext('2d')
 
-  return function (t) {
-  if ( t === void 0 ) t = 0;
+target.lineCap = 'round'
+target.strokeStyle = 'rgba(0, 0, 0, 0.1)'
 
-  return A * Math.cos((t * f) + p) * Math.exp(-d * t);
-;
-}  };
+const step = { x: canvas.width / 3, y: canvas.height / 2 }
+const cell = { x: step.x * 0.5, y: step.y * 0.5 }
+const size = cell.y * 0.75
 
-var canvas = document.querySelector('canvas');
-var target = canvas.getContext('2d');
+const driver = (A = 1, a = 1, b = a, phase = Math.PI, B = A) => {
+  const x = swing(A, a, phase)
+  const y = swing(B, b)
 
-target.lineCap = 'round';
-target.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+  return t => ({ x: x(t), y: y(t) })
+}
 
-var step = { x: canvas.width / 3, y: canvas.height / 2 };
-var cell = { x: step.x * 0.5, y: step.y * 0.5 };
-var size = cell.y * 0.75;
-
-var driver = function (A, a, b, phase, B) {
-  if ( A === void 0 ) A = 1;
-  if ( a === void 0 ) a = 1;
-  if ( b === void 0 ) b = a;
-  if ( phase === void 0 ) phase = Math.PI;
-  if ( B === void 0 ) B = A;
-
-  var x = swing(A, a, phase);
-  var y = swing(B, b);
-
-  return function (t) { return ({ x: x(t), y: y(t) }); }
-};
-
-var render = function (wave, t) {
-  if ( t === void 0 ) t = 0;
-
+const render = (wave, t = 0) => {
   if (t < 1) {
     return false
   }
 
-  var p = wave(t * 0.006);
+  const p = wave(t * 0.006)
 
-  target.beginPath();
-  target.moveTo(p.x, p.y);
-  target.lineTo(0, 0);
-  target.stroke();
+  target.beginPath()
+  target.moveTo(p.x, p.y)
+  target.lineTo(0, 0)
+  target.stroke()
 
   return render(wave, t - 1)
-};
+}
 
-var grid = function (v, i) { return ({ x: i % 3, y: Math.floor(i / 3) }); };
+const grid = (v, i) => ({ x: i % 3, y: Math.floor(i / 3) })
 
-Array.from({ length: 3 * 2 }).map(grid).forEach(function (v, i) {
-  var x = (v.x * step.x) + cell.x;
-  var y = (v.y * step.y) + cell.y;
+Array.from({ length: 3 * 2 }).map(grid).forEach((v, i) => {
+  const x = (v.x * step.x) + cell.x
+  const y = (v.y * step.y) + cell.y
 
   // Odd numbers repeated, A109613
-  var k = 2 * Math.floor(i * 0.5);
-  var a = k + 1;
+  const k = 2 * Math.floor(i * 0.5)
+  const a = k + 1
 
   // Flip on each turn
-  var b = i % 2 ? 2 : 4;
+  const b = i % 2 ? 2 : 4
 
-  var lookup = driver(size, a, b);
+  const lookup = driver(size, a, b)
 
-  target.save();
-  target.translate(x, y);
+  target.save()
+  target.translate(x, y)
 
-  render(lookup, 525);
+  render(lookup, 525)
 
-  target.restore();
-});
-
-}());
-
+  target.restore()
+})
